420696ed18f950541195bef6f5e125a02b14e7f1
irashinking <superbiagulaluxuxria@outlook.com>
2021-11-17 18:23:23 +0800

    Initial Commit

---
+++ OCProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCProject
+//
+//  Created by IraShinking on 2021/11/17.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
